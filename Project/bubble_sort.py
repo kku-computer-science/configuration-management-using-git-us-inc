8b026f1b0bdad4297330568eb49d50f48960298e
AusdawutNOTE <ausdawut.a@kkuamil.com>
2025-12-10 15:27:16 +0700

    Add bubble sort implementation (Ausdawut_0281)

---
+++ Project/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+
+    arr = arr.copy()
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
